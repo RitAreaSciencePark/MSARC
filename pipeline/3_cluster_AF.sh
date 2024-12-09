c0a4c93b684637c392c5c0d10a379974c73696e6
Valerio Piomponi <vpiomponi@login02.hpc.rd.areasciencepark.it>
2024-12-09 14:15:42 +0100

    step 3 updated, 4 not needed anymore

--- pipeline/3_cluster_AF.sh
+++ pipeline/3_cluster_AF.sh
@@ -17,3 +17,15 @@ do
     perl "scripts/reformat.pl" -v 0 -r fas a3m $input$msa $input$name".a3m"
     colabfold_batch  --amber --num-recycle 3 --num-relax 1 --num-models 5 --use-gpu-relax $input$name".a3m" $output$name
 done
+
+
+path=$name"/AF_clusters" # path_to_input_files (AF pdbs)
+output=$name"/output" # output directory
+mkdir -p "$output"
+
+
+awk '{ if ($12 != "H" ) print $0 }' $name/AF_full/*_relaxed_rank_001*  > $output/fullMSA_noH.pdb
+for i in $(ls $path | grep -oP 'cluster_\K\d+' | sort -n)
+do
+        awk '{ if ($12 != "H" ) print $0 }' $path/cluster_$i/cluster_${i}_relaxed_rank_001* > $output/clus$i.pdb
+done
